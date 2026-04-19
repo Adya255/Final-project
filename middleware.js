e61f229737f30156f7650e7ee818b3fa3a794a36
Adya sri <adyasri06@gmail.com>
2026-04-19 17:49:16 +0530

    fixed catwegory again

--- middleware.js
+++ middleware.js
@@ -25,7 +25,7 @@ module.exports.saveRedirectUrl=(req,res,next) =>{
 module.exports.isOwner= async(req,res,next)=>{
      let {id} =req.params;
      let listing=await Listing.findById(id);
-     if(!listing || !listing.owner.equals(res.locals.currentUser._id)){
+     if(!res.locals.currentUser || !listing.owner.equals(res.locals.currentUser._id)){
         req.flash("error","You don't have permission to edit");
         return res.redirect(`/listings/${id}`);
     }
@@ -34,7 +34,7 @@ module.exports.isOwner= async(req,res,next)=>{
 
 
 module.exports.validateListing=(req,res,next)=>{
-     let {error}=ListingSchema.validate(req.body.listing);
+     let {error}=ListingSchema.validate(req.body);
      if(error){
         let errMsg=error.details.map((el)=> el.message).join(",");
         throw new ExpressError(400,errMsg);
@@ -59,7 +59,7 @@ module.exports.validateReview=(req,res,next)=>{
 module.exports.isreviewAuthor= async(req,res,next)=>{
      let {id,reviewId} =req.params;
      let review=await Review.findById(reviewId);
-     if(!review || !review.author.equals(res.locals.currentUser._id)){
+     if(!res.locals.currentUser || !review.author.equals(res.locals.currentUser._id)){
         req.flash("error","You are not the author of this review");
         return res.redirect(`/listings/${id}`);
     }
